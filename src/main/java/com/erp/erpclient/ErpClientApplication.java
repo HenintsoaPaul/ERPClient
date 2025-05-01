3adca339b60e1dc8eedd688860fed781c34ae0dc
henintsoapaul <henintsoapaul@gmail.com>
2025-05-01 09:37:23 +0300

    init project

---
+++ src/main/java/com/erp/erpclient/ErpClientApplication.java
@@ -0,0 +1,13 @@
+package com.erp.erpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpClientApplication.class, args);
+    }
+
+}
