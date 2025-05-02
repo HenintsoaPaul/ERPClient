0877631f516f381a8ad7eb45f9a1ec7cdacbd29d
henintsoapaul <henintsoapaul@gmail.com>
2025-05-02 09:24:48 +0300

    entity: purchase invoice

---
+++ src/main/java/com/erp/erpclient/entity/pinvoice/PurchaseInvoiceAdvance.java
@@ -0,0 +1,37 @@
+package com.erp.erpclient.entity.pinvoice;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+//@Entity
+//@Table(name = "purchase_invoice_advance")
+public class PurchaseInvoiceAdvance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "reference_type")
+    private String referenceType;
+
+    @Column(name = "reference_name")
+    private String referenceName;
+
+    @Column(name = "remarks")
+    private String remarks;
+
+    @Column(name = "advance_amount", precision = 21, scale = 9)
+    private BigDecimal advanceAmount;
+
+    @Column(name = "allocated_amount", precision = 21, scale = 9)
+    private BigDecimal allocatedAmount;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "reference_date")
+    private Date referenceDate;
+
+    @ManyToOne
+    @JoinColumn(name = "purchase_invoice_id")
+    private PurchaseInvoice purchaseInvoice;
+}
