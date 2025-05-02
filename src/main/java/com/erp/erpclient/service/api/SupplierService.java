ab3ee1ff39e0d09a84f64f46b38997a803400293
henintsoapaul <henintsoapaul@gmail.com>
2025-05-02 21:19:01 +0300

    supplier-get list [fix]
    * centralize logic of RestClient builder
    * fix localDateTime parse issue

--- src/main/java/com/erp/erpclient/service/api/SupplierService.java
+++ src/main/java/com/erp/erpclient/service/api/SupplierService.java
@@ -2,43 +2,22 @@ package com.erp.erpclient.service.api;
 
 import com.erp.erpclient.SessionManager;
 import com.erp.erpclient.dto.SupplierResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class SupplierService {
 
     private final RestClient restClient;
-
-    @Autowired
     private final SessionManager sessionManager;
-
-    public SupplierService(SessionManager sessionManager) {
-        ObjectMapper mapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
-
-        this.restClient = RestClient.builder()
-                .baseUrl("http://library.localhost:8000")
-                .defaultHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
-                .messageConverters(converters -> {
-                    converters.add(0, new StringHttpMessageConverter());
-                    converters.add(1, converter);
-                })
-                .build();
-
-        this.sessionManager = sessionManager;
-    }
 
     public SupplierResponse findAll() {
         try {
