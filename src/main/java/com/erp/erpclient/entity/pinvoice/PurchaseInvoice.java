856b4552b08f5fb2a3d0d175f3fbe809ba9ce48e
henintsoapaul <henintsoapaul@gmail.com>
2025-05-04 20:39:00 +0300

    [func]-entity fix

--- src/main/java/com/erp/erpclient/entity/pinvoice/PurchaseInvoice.java
+++ src/main/java/com/erp/erpclient/entity/pinvoice/PurchaseInvoice.java
@@ -11,14 +11,12 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PurchaseInvoice extends Transaction {
-
     private String taxId;
     private Date dueDate;
     private Date postingDate;
     private String postingTime;
+    private String status;
 
-//    private Date transactionDate;
-//    private Date scheduleDate;
     private Boolean isPaid = false;
     private Boolean isReturn = false;
     private Boolean applyTds = false;
@@ -37,68 +35,4 @@ public class PurchaseInvoice extends Transaction {
     private Boolean updateStock = false;
 
     private List<PurchaseInvoiceItem> items;
-
-    private BigDecimal totalQty;
-    private BigDecimal totalNetWeight;
-    private BigDecimal baseTotal;
-    private BigDecimal baseNetTotal;
-    private BigDecimal total;
-    private BigDecimal netTotal;
-
-
-    private BigDecimal baseTaxesAndChargesAdded;
-    private BigDecimal baseTaxesAndChargesDeducted;
-    private BigDecimal baseTotalTaxesAndCharges;
-    private BigDecimal taxesAndChargesAdded;
-    private BigDecimal taxesAndChargesDeducted;
-    private BigDecimal totalTaxesAndCharges;
-    private String applyDiscountOn = "Grand Total";
-    private BigDecimal baseDiscountAmount;
-    private BigDecimal additionalDiscountPercentage;
-    private BigDecimal discountAmount;
-    private BigDecimal baseGrandTotal;
-    private BigDecimal baseRoundingAdjustment;
-    private BigDecimal baseRoundedTotal;
-    private String baseInWords;
-    private BigDecimal grandTotal;
-    private BigDecimal roundingAdjustment;
-    private BigDecimal roundedTotal;
-    private String inWords;
-    private Boolean disableRoundedTotal = false;
-    private BigDecimal totalAdvance;
-    private BigDecimal outstandingAmount;
-    private String modeOfPayment;
-    private String cashBankAccount;
-    private Date clearanceDate;
-    private BigDecimal paidAmount;
-    private BigDecimal basePaidAmount;
-    private BigDecimal writeOffAmount;
-    private BigDecimal baseWriteOffAmount;
-    private String writeOffAccount;
-    private String writeOffCostCenter;
-
-    private String tcName;
-    private String creditTo;
-    private String isOpening = "No";
-    private String againstExpenseAccount;
-
-    //    @NotBlank
-    private String status = "Draft";
-    private String interCompanyInvoiceReference;
-    private String remarks;
-    private Date fromDate;
-    private Date toDate;
-    private String autoRepeat;
-    private String taxWithholdingCategory;
-    private String unrealizedProfitLossAccount;
-    private String setFromWarehouse;
-    private BigDecimal perReceived;
-    private Boolean ignoreDefaultPaymentTermsTemplate = false;//
-
-    //    //@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<AdvanceTax> advanceTax;
-    private String subscription;
-    private Boolean isOldSubcontractingFlow = false;
-    private BigDecimal taxWithholdingNetTotal;
-    private BigDecimal baseTaxWithholdingNetTotal;
 }
