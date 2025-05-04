84254bf4ae373a8b38b8649a1c3902bae733cc5b
henintsoapaul <henintsoapaul@gmail.com>
2025-05-04 18:07:53 +0300

    [func][purchase order]-add services

---
+++ src/main/java/com/erp/erpclient/service/api/PurchaseOrderService.java
@@ -0,0 +1,52 @@
+package com.erp.erpclient.service.api;
+
+import com.erp.erpclient.SessionManager;
+import com.erp.erpclient.dto.PurchaseOrderResponse;
+import com.erp.erpclient.dto.SupplierResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PurchaseOrderService {
+
+    private final RestClient restClient;
+    private final SessionManager sessionManager;
+
+    public PurchaseOrderResponse findAllBySupplier(String supplier) {
+        try {
+            String sessionCookie = sessionManager.getAuthCookie();
+
+            RestClient.ResponseSpec responseSpec = this.restClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/api/method/erpnext.api.get_purchase_orders")
+                            .queryParam("supplier_name", supplier)
+                            .build()
+                    )
+                    .header(HttpHeaders.COOKIE, sessionCookie)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve();
+
+            return responseSpec
+                    .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        String body = res.getBody().toString();
+                        throw new RuntimeException("API client error: " + res.getStatusCode() + " - " + body);
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
+                        String body = res.getBody().toString();
+                        throw new RuntimeException("API server error: " + res.getStatusCode() + " - " + body);
+                    })
+                    .body(PurchaseOrderResponse.class);
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error : " + e.getMessage());
+        }
+    }
+}
